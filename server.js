8d77affcaa2f63acacc4575a272fac495193823e
Anonym-Bruker <github@modalsli.com>
2021-12-09 23:56:43 +0100

    Added sums, colors and multiple charts

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 var express = require('express');
-var app = express();
-
-var Request = require('request');
-
 const config = require('./config/common.json');
+const https = require('https');
 
-const Chart = require('chart.js');
+var app = express();
 
 var port = config.port;
 
@@ -18,129 +15,151 @@ var meteringpoints = {};
 var tariffPrice = {};
 var maxtime = {};
 
+var firstDayInMonth = setFirstDay(new Date());
+var firstDayInPrevMonth = setfirstPrevMonth(new Date());
+
+app.get('/noreload', function (req, res) {
+   res.render('index',
+       {
+          values,
+          labels,
+          meteringpoints,
+          tariffPrice,
+          maxtime,
+          firstDayInMonth
+       })
+})
+
 app.get('/', function (req, res) {
-   fetchData().then(result => {
-      if(result == "OK"){
-         res.render('index',
-             {
-                values,
-                labels,
-                meteringpoints,
-                tariffPrice,
-                maxtime
-             })
-      }
-      else{
-         logging("Return nok OK....");
-         res.render('index',
-             {
-                values,
-                labels,
-                meteringpoints,
-                tariffPrice,
-                maxtime
-             })
-      }
-   })
-   .finally()
+    var currentdate = new Date();
+    var toDate = addHours(3, currentdate);
+    toDate = toDate.toISOString();
+
+    var startDate = firstDayInPrevMonth.toISOString();
+    //var startDate = removeDays(30, currentdate);
+    //startDate = startDate.toISOString();
+    logging("StartDate:" + startDate + ", " + "ToDate:" + toDate);
+
+    var auth = "Bearer " + config.authkey;
+    var url = "https://elvia.azure-api.net/customer/metervalues/api/v1/metervalues?startTime="+startDate+"&endTime="+toDate+"&meteringPointIds="+config.meteringpointId+"&includeProduction=false";
+
+    (async () => {
+        https.get(url, {headers : {"Authorization" : auth}} , (resp) => {
+            let data = '';
+
+            // A chunk of data has been received.
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                meteringpoints = JSON.parse(data).meteringpoints[0].metervalue.timeSeries;
+                //logging("Done with getting metering data...." + JSON.stringify(meteringpoints));
+            });
+
+            (async () => {
+
+                url = "https://elvia.azure-api.net/customer/metervalues/api/v1/maxhours"
+
+                https.get(url, {headers : {"Authorization" : auth}} , (resp) => {
+                    let data = '';
+
+                    // A chunk of data has been received.
+                    resp.on('data', (chunk) => {
+                        data += chunk;
+                    });
+
+                    // The whole response has been received. Print out the result.
+                    resp.on('end', () => {
+                        maxtime = JSON.parse(data).meteringpoints[0].maxHours;
+                        //logging("Done with getting maxtime data...." + JSON.stringify(maxtime));
+                    });
+
+                    (async () => {
+                        // private_tou_daynight1 eller private_flatrate_house
+                        var subkey = config.subkey;
+                        var tariffkey = config.tariffkey;
+                        url = "https://elvia.azure-api.net/grid-tariff/api/1/tariffquery?TariffKey="+tariffkey+"&StartTime="+startDate+"&EndTime="+toDate;
+                        https.get(url, {headers : {"Ocp-Apim-Subscription-Key" : subkey}} , (resp) => {
+                            let data = '';
+
+                            // A chunk of data has been received.
+                            resp.on('data', (chunk) => {
+                                data += chunk;
+                            });
+
+                            // The whole response has been received. Print out the result.
+                            resp.on('end', () => {
+                                tariffPrice = JSON.parse(data).gridTariff.tariffPrice.priceInfo;
+                                //logging("Done with getting price data...." + JSON.stringify(tariffPrice));
+                                logging("Done with getting all data. Rendering page....");
+                                res.render('index',
+                                    {
+                                        values,
+                                        labels,
+                                        meteringpoints,
+                                        tariffPrice,
+                                        maxtime,
+                                        firstDayInMonth
+                                    })
+                            });
+                        }).on("error", (err) => {
+                            console.log("Error: " + err.message);
+                        });
+                    })();
+                }).on("error", (err) => {
+                    console.log("Error: " + err.message);
+                });
+            })();
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+        });
+    })();
 })
 
 
-fetchData();
-
-
 function removeDays(days, date) {
-   //var date = new Date(this.valueOf());
-   date.setDate(date.getDate() - days);
-   return date;
+    //var date = new Date(this.valueOf());
+    date.setDate(date.getDate() - days);
+    return date;
 }
 
+function setFirstDay(date) {
+    //var date = new Date(this.valueOf());
+    date.setMonth(date.getMonth());
+    date.setDate(1);
+    date.setHours(1)
+    date.setMinutes(0)
+    date.setSeconds(0)
+    date.setMilliseconds(0)
+    return date;
+}
+
+function setfirstPrevMonth(date) {
+    //var date = new Date(this.valueOf());
+    date.setMonth(date.getMonth()-1);
+    date.setDate(1);
+    date.setHours(0)
+    date.setMinutes(0)
+    date.setSeconds(0)
+    date.setMilliseconds(0)
+    return date;
+}
+
+
 function addHours(hours, date) {
-   //var date = new Date(this.valueOf());
-   date.setHours(date.getHours() + hours);
-   return date;
+    //var date = new Date(this.valueOf());
+    date.setHours(date.getHours() + hours);
+    return date;
 }
 
 function logging(logtext)
 {
-   //const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric'};
-   console.log(port + ":" + new Date().toISOString() + " : " + logtext);
-}
-
-
-async function fetchData(){
-
-
-   var currentdate = new Date();
-   var toDate = addHours(3, currentdate);
-   toDate = toDate.toISOString();
-
-   var startDate = removeDays(3, currentdate);
-   startDate = startDate.toISOString();
-   logging("StartDate:" + startDate + ", " + "ToDate:" + toDate);
-
-   var auth = "Bearer " + config.authkey;
-   var url = "https://elvia.azure-api.net/customer/metervalues/api/v1/metervalues?startTime="+startDate+"&endTime="+toDate+"&meteringPointIds="+config.meteringpointId+"&includeProduction=false";
-
-   Request.get( {
-      url : url,
-      headers : {
-         "Authorization" : auth
-      },
-      rejectUnauthorized: true,
-      requestCert: false,
-      agent: false
-   }, function(error, response, body) {
-      var response = JSON.parse(body);
-      error != null ? logging('***** ERROR ***** : ', error) : null;
-      meteringpoints = response.meteringpoints[0].metervalue.timeSeries;
-      //logging(JSON.stringify(meteringpoints))
-   } );
-
-   url = "https://elvia.azure-api.net/customer/metervalues/api/v1/maxhours"
-   Request.get( {
-      url : url,
-      headers : {
-         "Authorization" : auth
-      },
-      rejectUnauthorized: true,
-      requestCert: false,
-      agent: false
-   }, function(error, response, body) {
-      var response = JSON.parse(body);
-      error != null ? logging('***** ERROR ***** : ', error) : null;
-      maxtime = response.meteringpoints[0].maxHours;
-      //logging(JSON.stringify(maxtime))
-   } );
-
-
-
-   var subkey = config.subkey;
-   var tariffkey = config.tariffkey;
-   url = "https://elvia.azure-api.net/grid-tariff/api/1/tariffquery?TariffKey="+tariffkey+"&StartTime="+startDate+"&EndTime="+toDate;
-
-   Request.get( {
-      url : url,
-      headers : {
-         "Ocp-Apim-Subscription-Key" : subkey
-      },
-      rejectUnauthorized: true,
-      requestCert: false,
-      agent: false
-   }, function(error, response, body) {
-      var response = JSON.parse(body);
-      error != null ? logging('***** ERROR ***** : ', error) : null;
-      tariffPrice = response.gridTariff.tariffPrice.priceInfo;
-      //logging(tariffPrice.length);
-      //logging(JSON.stringify(tariffPrice));
-   } );
-
-
-
-   logging("Done with getting data....");
-   return "OK";
+    //const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric'};
+    console.log(port + ":" + new Date().toISOString() + " : " + logtext);
 }
 
 var server = app.listen(port, function () {
-   logging( "Server started, listening at port")
+    logging( "Server started, listening at port")
 })
